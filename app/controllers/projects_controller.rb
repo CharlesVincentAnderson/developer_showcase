64d82c966e2304038c9a8ddcc99c845d11b1ad28
chuck <charlesvincentanderson@gmail.com>
2021-01-12 16:46:13 -0500

    projects added"

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,55 @@
+class ProjectsController < ApplicationController
+  layout 'projects_layout'
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new project_params
+    count = 0
+    @project.portfolio_id = current_user.portfolio.id
+    portfolio = @project.portfolio
+    count = portfolio.projects.length
+#    for i in 0...portfolio.projects.length
+#      count = i if portfolio.projects[i] == @project
+#    end
+    if @project.save
+      redirect_to portfolio_path(current_user.portfolio, sub_view: "project,#{count}"), notice: "Project added successfully"
+    else
+      redirect_to portfolio_path(current_user.portfolio, sub_view: "project,#{count}"), alert: "Project could not be added"
+    end
+  end
+
+  def edit
+    @project = Project.find params[:id]  
+  end
+
+  def update
+    count = 0
+    @project = Project.find params[:id]
+    portfolio = @project.portfolio
+    for i in 0...portfolio.projects.length
+      count = i if portfolio.projects[i] == @project
+    end
+    if @project.update project_params
+      redirect_to portfolio_path(current_user.portfolio, sub_view: "project,#{count}"), notice: "Project updated successfully"
+    else
+      redirect_to portfolio_path(current_user.portfolio, sub_view: "project,#{count}"), alert: "Project could not be updated"
+    end
+  end
+
+  def destroy
+    project = Project.find params[:id]
+    if project.destroy
+      redirect_to portfolio_path(current_user.portfolio, sub_view: "about"), notice: "Project deleted successfully"
+    else
+      redirect_to portfolio_path(current_user.portfolio, sub_view: "about"), notice: "Project could not be deleted"
+    end
+  end
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :role, :github)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_032422) do
+ActiveRecord::Schema.define(version: 2020_12_07_021004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,18 @@ ActiveRecord::Schema.define(version: 2020_12_04_032422) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_portfolios_on_user_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.bigint "portfolio_id"
+    t.string "name"
+    t.string "description"
+    t.string "role"
+    t.string "github"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "website"
+    t.index ["portfolio_id"], name: "index_projects_on_portfolio_id"
   end
 
   create_table "users", force: :cascade do |t|
